893a40f46e88535d1a22882c868658c1220fce3c
Genn05 <ademjendoubi21@gmail.com>
2025-05-01 17:48:23 +0100

    Fonctionnalité de réinitialisation de mot de passe

---
+++ src/Form/ChangePasswordFormType.php
@@ -0,0 +1,30 @@
+<?php
+
+// src/Form/ChangePasswordFormType.php
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ChangePasswordFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('plainPassword', PasswordType::class, [
+            'label' => 'Nouveau mot de passe',
+            'mapped' => false, // ⚠️ important !
+            'attr' => ['autocomplete' => 'new-password'],
+            'constraints' => [
+                new NotBlank(['message' => 'Veuillez entrer un mot de passe']),
+                new Length([
+                    'min' => 6,
+                    'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                    'max' => 4096,
+                ]),
+            ],
+        ]);
+    }
+}
